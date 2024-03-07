2df82b28120c6bd55fd5a57002d54509bf64b5e6
Ainun Nadhifah Syamsiyah <58164571+ainunns@users.noreply.github.com>
2024-03-07 14:46:44 +0700

    feat: add typography component (#3)
    * feat: add font
    
    * feat: add typography design system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import './globals.css';
+import '@/styles/global.css';
 
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import { poppins } from '@/lib/font';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
