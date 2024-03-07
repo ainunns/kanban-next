35c26eedf32838b49256c687ac5dda05a2bfeaaa
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-03-07 20:55:07 +0700

    feat: add arrow links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
+import ArrowLink from '@/components/links/ArrowLink';
 import Typography from '@/components/Typography';
 
 export default function Home() {
   return (
     <main className='w-full'>
+      <ArrowLink href='/'>Go to the homepage</ArrowLink>
       <Typography as='h1' variant='h5' weight='bold'>
         Kanban board
       </Typography>
